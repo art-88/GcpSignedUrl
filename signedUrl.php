f2af661a816488b94d50a27dc4420b25f77b354a
Arata Sekine <arata.1203@gmail.com>
2018-03-18 02:38:59 +0900

    php xxxx.php xxxx.json bucket_name object_name で実行できるように変更

--- signedUrl.php
+++ signedUrl.php
@@ -1,11 +1,11 @@
 <?php
 require_once "vendor/autoload.php";
 try {
-    $path = __DIR__ . "/gcp_testuser_credentials.json";
+    $path = __DIR__ . "/" . $argv[1];
     putenv("GOOGLE_APPLICATION_CREDENTIALS=${path}");
     $storage = new \Google\Cloud\Storage\StorageClient();
-    $bucket = $storage->bucket("bucketName");
-    $object = $bucket->object("object.jpg");
+    $bucket = $storage->bucket($argv[2]);
+    $object = $bucket->object($argv[3]);
     return $object->signedUrl(time() + 180);
 } catch (Exception $e) {
     /** @noinspection PhpUnhandledExceptionInspection */
