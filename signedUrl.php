8a2f69b766ee3c27e0d7b0c1dc8405e39d1c87ce
Arata Sekine <arata.1203@gmail.com>
2018-03-18 02:43:56 +0900

    putenvしないように変更

--- signedUrl.php
+++ signedUrl.php
@@ -1,9 +1,7 @@
 <?php
 require_once "vendor/autoload.php";
 try {
-    $path = __DIR__ . "/" . $argv[1];
-    putenv("GOOGLE_APPLICATION_CREDENTIALS=${path}");
-    $storage = new \Google\Cloud\Storage\StorageClient();
+    $storage = new \Google\Cloud\Storage\StorageClient(['keyFilePath' => __DIR__ . "/" . $argv[1]]);
     $bucket = $storage->bucket($argv[2]);
     $object = $bucket->object($argv[3]);
     return $object->signedUrl(time() + 180);
